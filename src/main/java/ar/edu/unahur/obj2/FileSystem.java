727c4fd237f692ec1e3f390c076f79ad638a0f8e
Brandon duce <brandonduce12@gmail.com>
2025-10-01 17:38:07 -0300

    terminado

--- src/main/java/ar/edu/unahur/obj2/FileSystem.java
+++ src/main/java/ar/edu/unahur/obj2/FileSystem.java
@@ -1,5 +1,40 @@
 package ar.edu.unahur.obj2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class FileSystem {
+    private List<IElemento> listaElementos = new ArrayList<>();
+
+    public List<IElemento> getListaElementos() {
+        return listaElementos;
+    }
+
+    public FileSystem(List<IElemento> listaElementos) {
+        this.listaElementos = listaElementos;
+    }
+
+    public Integer tamanioTotalSistema(){
+        return listaElementos.stream().mapToInt(e -> e.tamanio()).sum();
+    }
+
+    public IElemento archivoMasPesado() {
+        return listaElementos.stream()
+        .map(IElemento::archivoMasPesado)
+        .max(Comparator.comparingInt(e -> e.tamanio()))
+        .orElse(null);
+    }
+
+    public void listar(){
+        listaElementos.stream().forEach(e -> e.mostrar(1));
+    }
+
+
+
+
+
+
+
 
 }
